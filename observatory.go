94497890a6fcb5ccecf4311188024357f7b9f330
Shahid Shah <github@shahidshah.com>
2018-06-24 07:59:03 -0400

    Initial commit

---
+++ observatory.go
@@ -0,0 +1,79 @@
+package observe
+
+import (
+	"context"
+	"io"
+	"log"
+
+	opentracing "github.com/opentracing/opentracing-go"
+
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+)
+
+// Observatory is a utility interface for dealing with common OpenTracing,
+// OpenMetrics, and other "observational" requirements in services and apps.
+type Observatory interface {
+	Tracer() opentracing.Tracer
+	Close()
+	StartTrace(subject string) opentracing.Span
+	StartChildTrace(subject string, parent opentracing.Span) opentracing.Span
+	StartTraceFromContext(ctx context.Context, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context)
+}
+
+// DefaultObservatory is a good default Observatory for the most common use
+// cases
+type DefaultObservatory struct {
+	tracer opentracing.Tracer
+	config jaegercfg.Configuration
+	closer io.Closer
+}
+
+func (o *DefaultObservatory) Tracer() opentracing.Tracer {
+	return o.tracer
+}
+
+func (o *DefaultObservatory) Close() {
+	if o.closer != nil {
+		o.closer.Close()
+	}
+}
+
+func (o *DefaultObservatory) StartTrace(subject string) opentracing.Span {
+	return o.tracer.StartSpan(subject)
+}
+
+func (o *DefaultObservatory) StartChildTrace(subject string, parent opentracing.Span) opentracing.Span {
+	return o.tracer.StartSpan(subject, opentracing.ChildOf(parent.Context()))
+}
+
+func (o *DefaultObservatory) StartTraceFromContext(ctx context.Context, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
+	var span opentracing.Span
+	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
+		opts = append(opts, opentracing.ChildOf(parentSpan.Context()))
+		span = o.tracer.StartSpan(operationName, opts...)
+	} else {
+		span = o.tracer.StartSpan(operationName, opts...)
+	}
+	return span, opentracing.ContextWithSpan(ctx, span)
+}
+
+func MakeDefaultObservatoryFromEnv() *DefaultObservatory {
+	result := new(DefaultObservatory)
+
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		// parsing errors might happen here, such as when we get a string where we expect a number
+		log.Printf("Could not parse Jaeger env vars: %s", err.Error())
+		return result
+	}
+	result.config = *cfg
+
+	tracer, closer, err := result.config.NewTracer()
+	if err != nil {
+		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
+		return result
+	}
+	result.tracer = tracer
+	result.closer = closer
+	return result
+}
